49ba125665e3b2e9b1ae0fcfd7f0cd4897819aa5
Lucas <iannuzzilucas@hotmail.com>
2022-10-06 20:10:25 -0300

    Desafio dotenv

---
+++ server.js
@@ -0,0 +1,242 @@
+const express = require("express");
+const { Server: HTTPServer, Server } = require("http");
+const { Server: SocketServer } = require("socket.io");
+const events = require("./socket_events");
+const messageDAO = require("./daos/messagesDao");
+const productDAO = require("./daos/productsDao");
+const usuarioDAO = require("./daos/usuariosDao");
+const { checkPassword, hashPassword } = require("./utils/utils");
+const { Types } = require("mongoose");
+const app = express();
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const httpServer = new HTTPServer(app);
+const socketServer = new SocketServer(httpServer);
+const handlebars = require("express-handlebars");
+const { Router } = express;
+const routerProducto = Router();
+const yargs = require("yargs");
+const args = yargs(process.argv.slice(2))
+  .alias({
+    p: "puerto",
+  })
+  .default({
+    puerto: 8080,
+  }).argv;
+
+const { fork } = require("child_process");
+
+app.use("/", routerProducto);
+routerProducto.use(express.static("public"));
+routerProducto.use(express.json());
+routerProducto.use(express.urlencoded({ extended: true }));
+const hbs = handlebars.create({
+  extname: ".hbs",
+  defaultLayout: "index.hbs",
+  layoutsDir: __dirname + "/public/views/layout",
+  partialsDir: __dirname + "/public/views/partials/",
+});
+app.engine("hbs", hbs.engine);
+
+app.set("views", "./public/views/partials");
+app.set("view engine", "hbs");
+
+passport.serializeUser((user, done) => {
+  console.log(user);
+  done(null, user._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  idUser = Types.ObjectId(id);
+  const user = await usuarioDAO.findUserById(idUser);
+  console.log(user);
+  done(null, user);
+});
+
+passport.use(
+  "login",
+  new LocalStrategy(async (username, password, done) => {
+    const usuario = await usuarioDAO.findUser(username);
+    const passHash = usuario.password;
+    if (!usuario || !checkPassword(password, passHash)) {
+      console.log(`Usuario y/o contraseña invalidos`);
+      return done(null, false);
+    } else {
+      return done(null, usuario);
+    }
+  })
+);
+
+passport.use(
+  "signup",
+  new LocalStrategy(
+    {
+      passReqToCallback: true,
+    },
+    async (req, username, password, done) => {
+      const usuario = await usuarioDAO.findUser(username);
+      if (usuario) {
+        return done(null, false, { message: "El usuario ya existe" });
+      }
+      const address = req.body.address;
+      const hashedPassword = hashPassword(password);
+      const newUser = {
+        username: username,
+        password: hashedPassword,
+        address: address,
+      };
+      const generateUser = await usuarioDAO.saveUser(newUser);
+      return done(null, generateUser);
+    }
+  )
+);
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+routerProducto.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://lucasiannu:wxRk2hMHkRguBXdU@cluster0.l96bh3b.mongodb.net/?retryWrites=true&w=majority",
+      ttl: 60,
+      mongoOptions: advancedOptions,
+    }),
+
+    secret: "secret_String",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+routerProducto.use(passport.initialize());
+routerProducto.use(passport.session());
+
+routerProducto.get("/login", (req, res) => {
+  res.render("logIn.hbs");
+});
+
+routerProducto.post(
+  "/login",
+  passport.authenticate("login", {
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect("/");
+  }
+);
+
+routerProducto.get("/signup", (req, res) => {
+  res.render("signUp.hbs");
+});
+
+routerProducto.post(
+  "/signup",
+  passport.authenticate("signup", {
+    failureRedirect: "/signup",
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect("/login");
+  }
+);
+
+let messages = "";
+
+routerProducto.get("/", async (req, res) => {
+  try {
+    const usuario = req.session.user;
+    const userExists = usuario ? true : false;
+    const allProducts = await productDAO.getAll();
+    if (allProducts.length > 0) {
+      res.render("main.hbs", {
+        allProducts: allProducts,
+        arrayExists: true,
+        userExists: userExists,
+        usuario: usuario,
+      });
+    } else {
+      res.render("main.hbs", {
+        allProducts: allProducts,
+        arrayExists: false,
+        userExists: userExists,
+        usuario: usuario,
+      });
+    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+routerProducto.get("/api/productos-test", async (req, res) => {
+  try {
+    const result = await productDAO.generateProducts(5);
+    res.json(result);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+routerProducto.get("/info", (req, res) => {
+  const memory = process.memoryUsage();
+  res.json({
+    "Argumentos de entrada": args._,
+    Plataforma: process.platform,
+    "Version de Node": process.version,
+    "Memoria Rss": memory.rss,
+    "Path de Ejecucion": process.execPath,
+    "Process Id": process.pid,
+    "Carpeta del Proyecto": process.cwd(),
+  });
+});
+
+routerProducto.get("/api/randoms", (req, res) => {
+  const child = fork("./fork/child.js");
+  const cantidad = req.query.cantidad ? req.query.cantidad : 1000000000;
+  child.send(cantidad);
+  child.on("message", (msg) => {
+    console.log(msg)
+    res.json({"Numeros Randoms:": msg})
+    res.end();
+  });
+});
+
+routerProducto.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.send("Error al cerrar sesion");
+    } else {
+      res.redirect("/login");
+    }
+  });
+});
+
+async function getMsg() {
+  try {
+    messages = await messageDAO.getAllMsg();
+  } catch (error) {}
+}
+
+getMsg();
+
+socketServer.on("connection", (socket) => {
+  console.log("Nuevo cliente conectado");
+  socketServer.emit("INIT", "Bienvenido al WebSocket", messages);
+
+  socket.on(events.POST_MESSAGE, (msg) => {
+    messageDAO.saveMsg(msg);
+    socketServer.sockets.emit(events.NEW_MESSAGE, msg);
+  });
+
+  socket.on(events.POST_PRODUCT, (product) => {
+    productDAO.saveProduct(product).then((response) => {
+      socketServer.sockets.emit(events.NEW_PRODUCT, product);
+    });
+  });
+});
+
+const PORT = args.p ? args.p : args.puerto;
+httpServer.listen(PORT, () => {
+  console.log(`Servidor escuchando el puerto ${PORT}`);
+});
